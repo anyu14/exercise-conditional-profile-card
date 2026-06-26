565807da7d1da7a8b26103c8774732f7e76794eb
anyu14 <anyu1999ramirez@gmail.com>
2026-06-26 00:13:23 +0000

    perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,18 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Nombre" : variables.name} ${
+    variables.lastName == null ? "Apellido" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Puesto" : variables.role}</h2>
+          <h3>${variables.city == null ? "Ciudad" : variables.city}, ${
+    variables.country == null ? "País" : variables.country
+  }</h3>
+         <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://github.com/anyu14"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/anyu-ramirez-969b8b337/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/anyu_cr/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
